c427d908ad9d9453f5961da6a3cc1461baee1c12
Amritanshu Dash <amritanshudash6677@gmail.com>
2024-06-03 16:10:00 +0530

    project initialisation

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Amritanshu Dash on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
